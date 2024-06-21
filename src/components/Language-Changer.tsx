12e9b9b128a038963910ac2e2fde8e6a3ff154d1
rahman-a <ahm.abdrahman@gamil.com>
2024-06-21 17:10:07 +0300

    make language changer component as dropdown component

--- src/components/Language-Changer.tsx
+++ src/components/Language-Changer.tsx
@@ -2,6 +2,14 @@
 import * as React from 'react'
 import { useLocale } from 'next-intl'
 import { useRouter } from 'next/navigation'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { cn } from '@/lib/utils'
 
 export interface ILanguageChangerProps {
@@ -13,23 +21,30 @@ export default function LanguageChanger({ className }: ILanguageChangerProps) {
   const router = useRouter()
   const locale = useLocale() as Locale
   const language = {
-    en: 'English',
-    ar: 'العربية',
+    ar: 'English',
+    en: 'العربية',
   }
   // change html direction based on locale
-  const changeDirection = () => {
-    document.cookie = `NEXT_LOCALE=${
-      locale === 'en' ? 'ar' : 'en'
-    }; path=/; max-age=31536000; SameSite=Lax`
+  const changeDirection = (locale: Locale) => {
+    document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=31536000; SameSite=Lax`
     router.push('/')
     router.refresh()
   }
   return (
-    <button
-      className={cn('bg-primary px-1 rounded-sm', className)}
-      onClick={changeDirection}
-    >
-      {language[locale]}
-    </button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <button className={cn('bg-primary px-1 rounded-sm', className)}>
+          {language[locale]}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => changeDirection('en')}>
+          English
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => changeDirection('ar')}>
+          العربية
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
